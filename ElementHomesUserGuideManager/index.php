4eba02f640864c526836ce9a2ce4cf526e8cb954
Neil Jackson <neil@family-jackson.co.uk>
2017-07-12 14:13:26 +0100

    testing cross on image

--- ElementHomesUserGuideManager/index.php
+++ ElementHomesUserGuideManager/index.php
@@ -1,5 +1,90 @@
-<?php
 
-echo "Hello World!";
+<html>
+<head>
+    <title>Add Floors</title>
+</head>
+<body>
 
-?>
+    <h1>What do you want to do?</h1>
+
+    <table width="25%" border="0">
+        <tr>
+            <td>Item</td>
+            <td>Add</td>
+            <td>Edit</td>
+            <td>Delete</td>
+            <td>List</td>
+        </tr>
+        <tr>
+            <td>Floors</td>
+            <td><a href="AddFloors.php">Add</a></td>
+            <td>
+                <a href="EditFloors.php">Edit</a>
+            </td>
+            <td>
+                <a href="DeleteFloors.php">Add</a>
+            </td>
+            <td>
+                <a href="IndexFloors.php">List</a>
+            </td>
+        </tr>
+
+        <tr>
+            <td>FloorPlans</td>
+            <td>
+                <a href="AddFloorPlans.php">Add</a>
+            </td>
+            <td>
+                <a href="EditFloorPlans.php">Edit</a>
+            </td>
+            <td>
+                <a href="DeleteFloorPlans.php">Add</a>
+            </td>
+            <td>
+                <a href="IndexFloorPlans.php">List</a>
+            </td>
+        </tr>
+
+        <tr>
+            <td>Rooms</td>
+            <td>
+                <a href="AddRooms.php">Add</a>
+            </td>
+            <td>
+                <a href="EditRooms.php">Edit</a>
+            </td>
+            <td>
+                <a href="DeleteRooms.php">Add</a>
+            </td>
+            <td>
+                <a href="IndexRooms.php">List</a>
+            </td>
+        </tr>
+        <tr>
+            <td>Locations</td>
+            <td>
+                <a href="AddLocations.php">Add</a>
+            </td>
+            <td>
+                <a href="EditLocations.php">Edit</a>
+            </td>
+            <td>
+                <a href="DeleteLocations.php">Add</a>
+            </td>
+            <td>
+                <a href="IndexLocations.php">List</a>
+            </td>
+        </tr>
+
+    </table>
+
+    <?php
+
+    include_once("Configure.php");
+
+    
+    ?>
+
+
+</body>
+</html>
